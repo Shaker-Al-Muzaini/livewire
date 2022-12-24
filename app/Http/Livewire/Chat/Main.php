774822ea86ad0b1085c3f9f6388d47923bff62ce
Shaker Al Muzaini <92122042+Shaker-Al-Muzaini@users.noreply.github.com>
2022-12-24 10:54:11 +0200

    Update Main.php

--- app/Http/Livewire/Chat/Main.php
+++ app/Http/Livewire/Chat/Main.php
@@ -2,12 +2,12 @@
 
 namespace App\Http\Livewire\Chat;
 
-use Livewire\Component;
+use Livewire\Component; 
 
 class Main extends Component
 {
-    public function render()
+    public function render() 
     {
         return view('livewire.chat.main');
-    }
+    } 
 }
