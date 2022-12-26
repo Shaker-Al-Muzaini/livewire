b3080ca5fff1ed5ad4091cc10dc55f9ed36d07b3
Shaker Al Muzaini <92122042+Shaker-Al-Muzaini@users.noreply.github.com>
2022-12-26 17:37:35 +0200

    Update MessageSent.php

--- app/Events/MessageSent.php
+++ app/Events/MessageSent.php
@@ -6,11 +6,11 @@ use App\Models\Conversation;
 use App\Models\Message;
 use App\Models\User;
 use Illuminate\Broadcasting\Channel;
-use Illuminate\Broadcasting\InteractsWithSockets; 
-use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\InteractsWithSockets;  
+use Illuminate\Broadcasting\PresenceChannel; 
 use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
-use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast; 
+use Illuminate\Foundation\Events\Dispatchable; 
 use Illuminate\Queue\SerializesModels;
 
 class MessageSent implements ShouldBroadcast 
